f4ecd3228d7daf58b4e88ee1bfe28359a4162485
Maria <mariacutipa.gonzales@gmail.com>
2017-03-15 15:17:03 -0500

    CSS

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load",function(){
-	var popup = document.getElementById("myPopup");
-	var a = document.getElementsByClassName("popup");
-
-	a.addEventListener("click",function(){
-		popup.classList.toggle("show");
-	});
-});
