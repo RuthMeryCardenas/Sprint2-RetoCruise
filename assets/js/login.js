4176bdf3016900cefd40d6154efe56e2684d9a12
Maria <mariacutipa.gonzales@gmail.com>
2017-03-16 13:01:59 -0500

    html css login

---
+++ assets/js/login.js
@@ -0,0 +1,10 @@
+//implementar
+window.addEventListener("load",function (){
+	var input = document.getElementsByClassName('form-input');
+	var login = document.getElementById('button-login');
+
+	login.addEventListener("click",function{
+
+	});
+})
+
