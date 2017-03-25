cbba25f2f18c067d4b1f09748fd810a13d2ed692
Maria <mariacutipa.gonzales@gmail.com>
2017-03-24 19:38:15 -0500

    Login

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,10 +1,25 @@
 //implementar
-window.addEventListener("load",function (){
-	var input = document.getElementsByClassName('form-input');
-	var login = document.getElementById('button-login');
+	var users = [{email : "mariacg@gmail.com",password : "123456"},
+				{email : "mery@laboratoria.la",password : "654321"},
+				{email : "maria@laboratoria.la",password : "987654"}];
 
-	login.addEventListener("click",function{
+document.getElementById('button-login').addEventListener("click",function(e){
+	var ema = document.getElementById('email').value;
+	var pwd = document.getElementById('pwd').value;	
+	var validation;
 
-	});
-})
+			users.forEach(function(e){
+				if (e.email == ema && e.password == pwd){
+					validation = true;
+				}
+			});
+
+			if (validation == true){window.location = "index.html";}
+			else{alert("Datos incorrectos");}
+	e.preventDefault();	
+});
+
+
+
+
 
