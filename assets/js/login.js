314e0d070e338836cf90ac85702fdcfb66bddd44
Maria <mariacutipa.gonzales@gmail.com>
2017-03-29 06:53:44 -0500

    Login

--- assets/js/login.js
+++ assets/js/login.js
@@ -4,18 +4,22 @@
 				{email : "maria@laboratoria.la",password : "987654"}];
 
 document.getElementById('button-login').addEventListener("click",function(e){
-	var ema = document.getElementById('email').value;
-	var pwd = document.getElementById('pwd').value;	
+	var ema = document.getElementById('email');
+	var pwd = document.getElementById('pwd');
 	var validation;
 
 			users.forEach(function(e){
-				if (e.email == ema && e.password == pwd){
+				if (e.email == ema.value && e.password == pwd.value){
 					validation = true;
 				}
 			});
 
 			if (validation == true){window.location = "index.html";}
-			else{alert("Datos incorrectos");}
+			else{
+				 ema.value="";
+				 pwd.value="";
+				 document.getElementById('result').style.display='block';
+				 }
 	e.preventDefault();	
 });
 
